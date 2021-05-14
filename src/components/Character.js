b735153f89661d642f1f682f932d71029e8989e2
djaden927 <djaden92@gmail.com>
2021-05-14 11:36:16 -0600

    passing tests, need to style and refactor

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,38 @@
 // Write your Character component here
+import React, { useState } from "react";
+import styled from 'styled-components'
+
+const CardContainer = styled.div
+`
+display:flex;
+flex-wrap: wrap;
+max-width: 100%;
+justify-content: space-evenly;
+border: 2px solid green;
+`
+
+const CardDiv = styled.div`
+width: 26%;
+border: 2px solid red;
+`
+
+
+export default function Character(props){
+    const {starWarsData} = props
+    console.log(starWarsData)
+    return(
+        // map
+        <CardContainer>
+        
+            {starWarsData.map((char) => {
+                return(
+                <CardDiv>
+                <h1>{char.name}</h1>
+                </CardDiv>
+                )
+            })}
+
+        
+        </CardContainer>
+    )
+}
